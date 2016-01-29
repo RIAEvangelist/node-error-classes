cbacb802f92497eb78cedb64d0a86a8234be2d3d
Brandon Nozaki Miller <brandon@diginow.it>
2016-01-29 00:46:09 -0800

    prepping version for npm

--- Errors.js
+++ Errors.js
@@ -5,7 +5,9 @@
  *
  * ` npm install node-error-classes `
  *
- Easy to read custom and extensible error classes for node.js that extend the native error classes.
+ Easy to read isomorphic, custom and extensible error classes for node.js and the browser that extend the native error classes.
+
+ This will work in the browser too with webpack, browserify, or any other common.js module loader!
 
  Sometimes, just throwing an error isn't that helpful. Sometimes, handling an error elegantly preventing a crash just isn't quite good enough. During team development there are times when you want to throw explicit errors to both help developers understand whats going on, as well as enforce some strict rules. That is what this module is for.
  *
