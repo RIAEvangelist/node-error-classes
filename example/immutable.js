1193fcaf040ac0bcf044f0fdb9a079add04f7229
Brandon Nozaki Miller <brandon@diginow.it>
2016-02-01 16:37:04 -0800

    immutable

--- example/immutable.js
+++ example/immutable.js
@@ -2,20 +2,50 @@
 
 const Errors = require('../Errors.js');
 
-function populateUser(){
-    let user = {
-        name : 'Mike',
-        age: 99,
-    }
-    if(typeof user.age != 'undefined'){
-        const err = new Errors.Immutable;
-        err.setMessage(
-            'user.age',
-            'pupulateUser'
-        );
-        throw err;
-    }
-    user.age = 45;
+class User{
+   constructor(name,age){
+       Object.defineProperties(
+           this,
+           {
+               age:{
+                   enumerable:true,
+                   writable:true,
+                   value:age
+               },
+               name:{
+                   enumerable:true,
+                   get:getName,
+                   set:setName
+               }
+           }
+       );
+
+       let userName=null;
+
+       function getName(){
+           return userName;
+       }
+
+       function setName(value){
+           if(userName){
+               const err=new Errors.Immutable;
+               err.setMessage(
+                   'name',
+                   this
+               );
+
+               throw err;
+           }
+           userName=value;
+           return userName;
+       }
+
+       if(name){
+           this.name=name;
+       }
+   }
 }
 
-populateUser();
+const bob=new User('bob',42);
+
+bob.name='bob';
